554d6472eb700db463232212038352f6abb4569d
AlishaBalamwar <alishabalamwar7806@gmail.com>
2022-01-02 15:00:48 +0530

    Added: add edit components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import HeaderComponents from './components/HeaderComponents/HeaderComponent';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import "./components/HeaderComponents/HeaderComponent.css"
+import HomeComponents from './components/HomeComponents/HomeComponent';
+import AddEditPayrollComponents from './components/AddEditPayrollComponents/AddEditPayrollComponent';
+import "./components/AddEditPayrollComponents/AddEditPayrollComponent.css"
+import "./components/HomeComponents/HomeComponent.css"
 
 function App() {
   return (
     <div className="App">
       <HeaderComponents/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<HomeComponents/>}></Route>
+          <Route path='/add-update' exact element={<AddEditPayrollComponents/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/HeaderComponents/HeaderComponent.jsx
+++ src/components/HeaderComponents/HeaderComponent.jsx
@@ -1,4 +1,4 @@
-import react, {Component} from 'react'
+import react, { Component } from 'react'
 
 export class HeaderComponent extends Component {
     render() {
@@ -24,4 +24,4 @@ export class HeaderComponent extends Component {
     }
 }
 
-export default HeaderComponent   
+export default HeaderComponent
